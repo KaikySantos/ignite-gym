2db3825655ac8bba1e7714312c7849fc11468ab2
kaikySantos <info.kaiky@gmail.com>
2023-01-26 15:28:08 -0300

    feat: component - group

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,11 +1,33 @@
-import { VStack } from "native-base";
+import { useState } from 'react';
+import { VStack, FlatList } from "native-base";
 
 import { HomeHeader } from "@components/HomeHeader";
+import { Group } from "@components/Group";
 
 export function Home() {
+  const [groups, setGroups] = useState(['Costas', 'Bíceps', 'Tríceps', 'Ombro']);
+  const [groupSelected, setGroupSelected] = useState('Costas');
+
   return (
     <VStack flex={1}>
       <HomeHeader />
+
+      <FlatList
+        data={groups}
+        keyExtractor={item => item}
+        renderItem={({ item }) => (
+          <Group
+            name={item}
+            isActive={groupSelected === item}
+            onPress={() => setGroupSelected(item)}
+          />
+        )}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        _contentContainerStyle={{ px: 8 }}
+        my={10}
+        maxH={10}
+      />
     </VStack>
   )
 }
