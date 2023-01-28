d6b7e6fffff157557b0e0d33cbde3e5f49b235a2
kaikySantos <info.kaiky@gmail.com>
2023-01-28 11:59:27 -0300

    feat preview upload image

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -12,14 +12,29 @@ const PHOTO_SIZE = 33;
 
 export function Profile() {
   const [photoIsLoading, setPhotoIsLoading] = useState(false);
+  const [userPhoto, setUserPhoto] = useState<string | undefined>('https://github.com/kaikySantos.png');
 
   async function handleUserPhotoSelect() {
-    await ImagePicker.launchImageLibraryAsync({
-      mediaTypes: ImagePicker.MediaTypeOptions.Images,
-      quality: 1,
-      aspect: [4, 4],
-      allowsEditing: true,
-    });
+    setPhotoIsLoading(true);
+
+    try {
+      const photoSelected = await ImagePicker.launchImageLibraryAsync({
+        mediaTypes: ImagePicker.MediaTypeOptions.Images,
+        quality: 1,
+        aspect: [4, 4],
+        allowsEditing: true,
+      });
+  
+      if (photoSelected.canceled) {
+        return;
+      }
+  
+      setUserPhoto(photoSelected.assets[0].uri);
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setPhotoIsLoading(false);
+    }
   }
 
   return (
@@ -39,7 +54,7 @@ export function Profile() {
               />
             :
             <UserPhoto
-              source={{ uri: 'https://github.com/kaikySantos.png' }}
+              source={{ uri: userPhoto }}
               alt="Foto do usuário"
               size={PHOTO_SIZE}
             />
