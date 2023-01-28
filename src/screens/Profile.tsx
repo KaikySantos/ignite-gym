638304b4339c36ef1c2776b5ad7f1fc234bc7aa1
kaikySantos <info.kaiky@gmail.com>
2023-01-28 12:16:06 -0300

    feat: profile page - upload user photo

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -1,7 +1,8 @@
 import { useState } from 'react';
-import { TouchableOpacity } from 'react-native';
-import { Center, ScrollView, VStack, Skeleton, Text, Heading } from "native-base";
+import { Alert, TouchableOpacity } from 'react-native';
+import { Center, ScrollView, VStack, Skeleton, Text, Heading, useToast } from "native-base";
 import * as ImagePicker from "expo-image-picker";
+import * as FileSystem from "expo-file-system";
 
 import { ScreenHeader } from "@components/ScreenHeader";
 import { UserPhoto } from "@components/UserPhoto";
@@ -13,6 +14,8 @@ const PHOTO_SIZE = 33;
 export function Profile() {
   const [photoIsLoading, setPhotoIsLoading] = useState(false);
   const [userPhoto, setUserPhoto] = useState<string | undefined>('https://github.com/kaikySantos.png');
+
+  const toast = useToast();
 
   async function handleUserPhotoSelect() {
     setPhotoIsLoading(true);
@@ -29,7 +32,19 @@ export function Profile() {
         return;
       }
   
-      setUserPhoto(photoSelected.assets[0].uri);
+      if (photoSelected.assets[0].uri) {
+        const photoInfo = await FileSystem.getInfoAsync(photoSelected.assets[0].uri);
+
+        if (photoInfo.size && (photoInfo.size / 1024 / 1024) > 5) {
+          return toast.show({
+            title: "Essa imagem é muito grande. Escolha uma de até 5MB.",
+            placement: 'top',
+            bgColor: 'red.500',
+          });
+        }
+
+        setUserPhoto(photoSelected.assets[0].uri);
+      }
     } catch (error) {
       console.log(error);
     } finally {
