1470f5b86585a1b8ac3aa8cd94954b8b45ff3e65
kaikySantos <info.kaiky@gmail.com>
2023-02-04 14:55:59 -0300

    feat: auth context

--- App.tsx
+++ App.tsx
@@ -2,6 +2,8 @@ import { StatusBar, Text, View } from 'react-native';
 import { NativeBaseProvider } from 'native-base';
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
 
+import { AuthContextProvider } from '@contexts/AuthContext';
+
 import { THEME } from './src/theme';
 
 import { Routes } from '@routes/index';
@@ -19,7 +21,9 @@ export default function App() {
         translucent
       />
 
-      {fontsLoaded ? <Routes /> : <Loading />}
+      <AuthContextProvider>
+        {fontsLoaded ? <Routes /> : <Loading />}
+      </AuthContextProvider>
     </NativeBaseProvider>
   );
 }
---
+++ src/contexts/AuthContext.tsx
@@ -0,0 +1,39 @@
+import { createContext, ReactNode, useState } from "react";
+
+import { UserDTO } from "@dtos/UserDTO";
+
+import { api } from "@services/api";
+
+export type AuthContextDataProps = {
+  user: UserDTO;
+  signIn: (email: string, password: string) => Promise<void>;
+}
+
+type AuthContextProvider = {
+  children: ReactNode;
+}
+
+export const AuthContext = createContext<AuthContextDataProps>({} as AuthContextDataProps);
+
+export function AuthContextProvider({ children }: AuthContextProvider) {
+  const [user, setUser] = useState<UserDTO>({} as UserDTO);
+
+  async function signIn(email: string, password: string) {
+    try {
+      const { data } = await api.post('/sessions', { email, password });
+  
+      if (data.user) {
+        setUser(data.user);
+        console.log(data.user);
+      }
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  return (
+    <AuthContext.Provider value={{ user, signIn }}>
+      {children}
+    </AuthContext.Provider>
+  )
+}
